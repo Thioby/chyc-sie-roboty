13ac581de76b64d3e96fc9a1867cce35d04fc243
Dawid Krysiński <dawid.krysinski@iteo.com>
2019-09-28 13:04:21 +0200

    Basic infrastructure

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:chyc_sie_roboty/presentation/sign_in/sign_in_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'ChycSieRoboty',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: SignInPage(),
+      );
+}
---
+++ lib/presentation/sign_in/sign_in_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  State createState() => _SignInState();
+}
+
+class _SignInState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold();
+}
