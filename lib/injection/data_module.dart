b4257f5b583c45ab63abd3e5c22a8c8de9ede46b
Lukasz Koszentka <lukaszkoszentka@gmail.com>
2019-09-28 21:00:54 +0200

    Finish logic

--- lib/injection/data_module.dart
+++ lib/injection/data_module.dart
@@ -1,4 +1,5 @@
 import 'package:chyc_sie_roboty/domain/auth/user_repository.dart';
+import 'package:chyc_sie_roboty/domain/use_case/sign_in_use_case.dart';
 import 'package:chyc_sie_roboty/infrastructure/auth/firebase_auth_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:injector/injector.dart';
@@ -9,5 +10,6 @@ registerDataModules(Injector injector) async {
   injector
     ..registerSingleton((_) => prefs)
     ..registerSingleton((_) => Firestore.instance)
-    ..registerSingleton<UserRepository>((injector) => FirebaseUserRepository(injector.getDependency()));
+    ..registerSingleton<UserRepository>((injector) => FirebaseUserRepository(injector.getDependency()))
+    ..registerDependency((injector) => SignInUseCase(injector.getDependency()));
 }
--- lib/style/app_colors.dart
+++ lib/style/app_colors.dart
@@ -2,6 +2,6 @@ import 'dart:ui';
 
 class AppColors {
   static const Color red = Color(0xFF0000);
-  static const Color azure = Color(0x00A8FF);
-  static const Color disabled = Color(0xf4f4f4);
+  static const Color azure = Color(0xFF00A8FF);
+  static const Color disabled = Color(0xFFf4f4f4);
 }
