ae0acadae71e2aa323330d3b8897ac1572900e12
Dawid Krysiński <dawid.krysinski@iteo.com>
2019-09-28 13:14:07 +0200

    Add styles

---
+++ lib/style/app_colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color red = Color(0xFF0000);
+}
---
+++ lib/style/app_typography.dart
@@ -0,0 +1,10 @@
+import 'package:chyc_sie_roboty/style/app_colors.dart';
+import 'package:flutter/cupertino.dart';
+
+class AppTypography {
+  static const titleTextStyle = TextStyle(
+    fontSize: 22,
+    color: AppColors.red,
+    fontWeight: FontWeight.w600,
+  );
+}
