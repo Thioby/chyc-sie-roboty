c9f7e56e982a24d0c9d3390fc07d910970dac00d
Dawid Krysiński <dawid.krysinski@iteo.com>
2019-09-28 11:52:36 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/pl/com/chycsieroboty/chyc_sie_roboty/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.com.chycsieroboty.chyc_sie_roboty
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
