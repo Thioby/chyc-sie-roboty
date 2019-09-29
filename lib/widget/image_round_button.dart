006e0df5bc275be149d028a3cda82df681d1ebf7
Dawid Krysiński <dawid.krysinski@iteo.com>
2019-09-29 07:39:48 +0200

    Home page adjustments

--- lib/widget/image_round_button.dart
+++ lib/widget/image_round_button.dart
@@ -1,19 +1,28 @@
+import 'package:chyc_sie_roboty/style/app_typography.dart';
+import 'package:chyc_sie_roboty/style/dimens.dart';
 import 'package:flutter/material.dart';
 
 class RoundImageButton extends StatelessWidget {
   final Widget image;
   final Function onTap;
+  final String text;
 
-  const RoundImageButton({Key key, this.image, this.onTap}) : super(key: key);
+  const RoundImageButton({Key key, this.image, this.onTap, this.text}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-        ),
-        child: GestureDetector(
-          child: image,
-          onTap: onTap,
-        ),
+  Widget build(BuildContext context) => Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: GestureDetector(
+              child: image,
+              onTap: onTap,
+            ),
+          ),
+          SizedBox(height: Dimens.S),
+          Text(text, style: AppTypography.buttonTextStyle),
+        ],
       );
 }
